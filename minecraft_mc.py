5e20d832535c17fa5d1286742a4f16aba1455669
NoobXD10 <143030149+NoobXD10@users.noreply.github.com>
2024-02-05 20:30:21 +0100

    Create minecraft_mc.py
    ...WEBMC MARKETPLACE ITEMS

---
+++ minecraft_mc.py
@@ -0,0 +1,7 @@
+# minecraft_mc.py
+
+from marketplace_mc import marketplace_screen
+
+# Example usage with a list of marketplace items
+marketplace_items = ["Item A", "Item B", "Item C"]
+marketplace_screen(marketplace_items)
