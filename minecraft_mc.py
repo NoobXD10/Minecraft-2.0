6652d97841f0d27a34007ec50a19786dd45c92dd
NoobXD10 <143030149+NoobXD10@users.noreply.github.com>
2024-02-05 20:31:58 +0100

    Update minecraft_mc.py

--- minecraft_mc.py
+++ minecraft_mc.py
@@ -3,5 +3,5 @@
 from marketplace_mc import marketplace_screen
 
 # Example usage with a list of marketplace items
-marketplace_items = ["Item A", "Item B", "Item C"]
+marketplace_items = ["Alex", "Solumbos", "Jutho"]
 marketplace_screen(marketplace_items)
