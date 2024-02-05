2566a48458e2a249738578c05f71e5a2b78ea00d
NoobXD10 <143030149+NoobXD10@users.noreply.github.com>
2024-02-05 20:35:13 +0100

    Update server.py
    server ip or multiplayer

--- server.py
+++ server.py
@@ -10,16 +10,37 @@ server.listen()
 worlds = {}
 
 def handle_client(client_socket, world_code):
-    # Handle client logic here
-    pass
+    try:
+        # Notify all clients in the world about the new player
+        for player_socket in worlds[world_code]["players"]:
+            player_socket.send(f"Player joined the world.".encode('utf-8'))
+
+        # Add the player's socket to the list of players in the world
+        worlds[world_code]["players"].append(client_socket)
+
+        while True:
+            # Receive and broadcast messages to other players
+            message = client_socket.recv(1024).decode('utf-8')
+
+            if message:
+                print(f"Received message from {world_code}: {message}")
+
+                for player_socket in worlds[world_code]["players"]:
+                    if player_socket != client_socket:
+                        player_socket.send(f"Player: {message}".encode('utf-8'))
+
+    except:
+        print("An error occurred.")
+        client_socket.close()
 
 while True:
     client, addr = server.accept()
-    world_code = "ABC123"  # Generate a unique code for the world
-    worlds[world_code] = {"players": []}
+    world_code = client.recv(1024).decode('utf-8')
 
-    # Send the world code to the client
-    client.send(world_code.encode())
+    if world_code in worlds:
+        worlds[world_code]["players"].append(client)
+    else:
+        worlds[world_code] = {"players": [client]}
 
     # Start a new thread to handle the client
     client_handler = threading.Thread(target=handle_client, args=(client, world_code))
